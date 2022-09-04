fa694ec7bf7fde6875d250738ed5c1a27a283ea4
buumu8 <buumu8@gmail.com>
2022-09-04 16:43:20 +0700

    adding hero section

--- navbar-header/script.js
+++ navbar-header/script.js
@@ -3,8 +3,9 @@ const header__meta = document.getElementById("header__meta");
 const header__categories = document.getElementById("header__categories");
 const header__nav = document.getElementById("header__nav");
 const logo = document.getElementById("mobile-logo");
+const header = document.getElementById("header");
 
-const array = [logo, btn, header__categories, header__meta, header__nav];
+const array = [logo, btn, header, header__categories, header__meta, header__nav];
 btn.addEventListener("click", () => {
   array.forEach((el) => el.classList.toggle("open"));
 });
