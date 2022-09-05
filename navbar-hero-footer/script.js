6c73635e8d7a6c45e0cfde231eeb7e7075552596
buumu8 <buumu8@gmail.com>
2022-09-05 13:28:10 +0700

    finalize product calousel

--- navbar-hero-footer/script.js
+++ navbar-hero-footer/script.js
@@ -6,6 +6,14 @@ const logo = document.getElementById("mobile-logo");
 const header = document.getElementById("header");
 
 const array = [logo, btn, header, header__categories, header__meta, header__nav];
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 btn.addEventListener("click", () => {
   array.forEach((el) => el.classList.toggle("open"));
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
